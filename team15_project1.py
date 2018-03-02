e49e34aa74a2c5a6e9b3344f822727cf971e77e9
RickJewell <derrick.jewell@g.austincc.edu>
2018-03-02 15:54:32 -0600

    Third run at the project. Reprints the instruction in a easier to read manner and adds the opcode/fcode and some register/values. Still needs: Implement BNE, MUL, SRL, JR, MOVZ, AND, OR, XOR After the break instruct switch to data processing. Flag? New func? Implement a two's complement converter to check immediate/data print to file rather than console

--- team15_project1.py
+++ team15_project1.py
@@ -15,7 +15,11 @@ binMem = []  # <type 'list'>: ['11111111111111111111111111111111', '111111111111
 valid = []
 opcode_list = []
 opcode_dictionary = {2: "J", 3: "LW", 5: "BNE", 6: "BLEZ", 8: "ADDI", 11: "SW", 28: "MUL" }
-fcodes_dictionary = {0: "SLL", 2: "SRL", 8: "JR", 10: "MOVZ", 32: "ADD", 34: "SUB", 36: "AND", 37: "OR", 38: "XOR"}
+fcodes_dictionary = {0: "SLL", 2: "SRL", 8: "JR", 10: "MOVZ", 13:"BREAK", 32: "ADD", 34: "SUB", 36: "AND", 37: "OR", 38: "XOR"}
+registers = {
+	 0:"R0",1:"R1",2:"R2",3:"R3",4:"R4",5:"R5",6:"R6",7:"R7",8:"R8",9:"R9",10:"R10",11:"R11",12:"R12",13:"R13",14:"R14",15:"R15",
+	16:"R16",17:"R17",18:"R18",19:"R19",20:"R20",21:"R21",22:"R22",23:"R23",24:"R24",25:"R25",26:"R26",27:"R27",28:"R28",29:"R29",30:"R30",31:"R31"
+}
 
 
 class Dissemble:
@@ -42,27 +46,41 @@ class Dissemble:
             output += i + " "
         return output
 
-    def Disassemble(self,instruction):
+    def Disassemble(self,instruction,program_counter):
         output = ""
         #Add PC here?
+        output += self.parse_instruction(instruction)
+        output += " " + str(program_counter).ljust(3)
+        output += "    "
+
         if(instruction >> 31 == 0):
             validStr.append("N")
-            output += self.parse_instruction(instruction)
             output += "Invalid Instruction"
             return output
         else:
-            output += self.parse_instruction(instruction)
             validStr.append("Y")
 
         opcode = (instruction & 0b01111100000000000000000000000000) >> (32 - 6)
+        rs =     (instruction & 0b00000011111000000000000000000000) >> (32 - 11)
+        rt =     (instruction & 0b00000000000111110000000000000000) >> (32 - 16)
+        rd =     (instruction & 0b00000000000000001111100000000000) >> (32 - 21)
+        sa =     (instruction & 0b00000000000000000000011111000000) >> (32 - 26)
+        fcode =  (instruction & 0b00000000000000000000000000111111)
+        #needs converted from two's complement
+        immediate = (instruction & 0b00000000000000001111111111111111)
 
         if(opcode == 2):
             opcode_list.append(opcode_dictionary[opcode])
             output += opcode_dictionary[opcode]
+            target = (instruction & 0b00000011111111111111111111111111) << 2
+            output += "     " + "#" + str(target)
             return output
         elif (opcode == 3):
             opcode_list.append(opcode_dictionary[opcode])
             output += opcode_dictionary[opcode]
+            output += "    "
+            output += registers[rt] + ", "
+            output += str(immediate) + "(" + registers[rs] + ")"
             return output
 
         elif (opcode == 5):
@@ -73,16 +91,26 @@ class Dissemble:
         elif (opcode == 6):
             opcode_list.append(opcode_dictionary[opcode])
             output += opcode_dictionary[opcode]
+            output += "  "
+            output += registers[rs] + ", "
+            output += "#" + str(immediate << 2)
             return output
 
         elif (opcode == 8):
             opcode_list.append(opcode_dictionary[opcode])
             output += opcode_dictionary[opcode]
+            output += "  "
+            output += registers[rs] + ", "
+            output += registers[rt] + ", "
+            output += "#" + str(immediate)
             return output
 
         elif (opcode == 11):
             opcode_list.append(opcode_dictionary[opcode])
             output += opcode_dictionary[opcode]
+            output += "    "
+            output += registers[rt] + ", "
+            output += str(immediate) + "(" + registers[rs] + ")"
             return output
 
         elif (opcode == 28):
@@ -96,6 +124,10 @@ class Dissemble:
             #SLL r0,r0,r0 expressed as NOP
             if(fcode == 0):
                 output += fcodes_dictionary[fcode]
+                output += "   "
+                output += registers[rd] + ", "
+                output += registers[rt] + ", "
+                output += "#" + str(sa)
                 return output
             if (fcode == 2):
                 output += fcodes_dictionary[fcode]
@@ -106,11 +138,22 @@ class Dissemble:
             if (fcode == 10):
                 output += fcodes_dictionary[fcode]
                 return output
+            if(fcode == 13):
+                output += fcodes_dictionary[fcode]
+                return output
             if (fcode == 32):
                 output += fcodes_dictionary[fcode]
+                output += "   "
+                output += registers[rd] + ", "
+                output += registers[rs] + ", "
+                output += registers[rt]
                 return output
             if (fcode == 34):
                 output += fcodes_dictionary[fcode]
+                output += "   "
+                output += registers[rd] + ", "
+                output += registers[rs] + ", "
+                output += registers[rt]
                 return output
             if (fcode == 36):
                 output += fcodes_dictionary[fcode]
@@ -154,11 +197,13 @@ class Dissemble:
             elif (sys.argv[i] == '-o' and i < (len(sys.argv) - 1)): \
                     outputFileName = sys.argv[i + 1]
 
+        program_counter = 96
         with open(inputFileName, "r") as Input_File:
             for line in Input_File:
                 dissassembled_line = ""
                 instruction = int(line,2)
-                dissassembled_line += self.Disassemble(instruction)
+                dissassembled_line += self.Disassemble(instruction,program_counter)
+                program_counter += 4
                 print(dissassembled_line)
 
 
